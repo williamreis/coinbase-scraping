b07c7eb6e4489b5c42606e5b25dd1e20ace457ef
williamreis <wjsreis@gmail.com>
2024-06-30 11:42:01 -0400

    chore: Rename Class

--- libs/coinbase.py
+++ libs/coinbase.py
@@ -3,10 +3,9 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from time import sleep
-from . import utils
 
 
-def scraping():
+def ExternalMarket():
     options = Options()
     # options.add_argument('--headless')
     options.add_argument('--no-sandbox')
@@ -51,4 +50,3 @@ def scraping():
         value_percentage += format_text_percentage
 
     print(f"CoinBase: Nas últimas 24 horas, o mercado está com o valor: {value_percentage}")
-
